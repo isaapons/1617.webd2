d556cd63126ea6b2afafed08b92c161482498d2b
isaapons <isaapons@student.arteveldehs.be>
2017-04-19 17:11:58 +0200

    help

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
 ;(function() {
 
-  document.getElementById("hamburger")
-  .addEventListener("click", function(){
-  var pressed = document.querySelectorAll(".main-nav > a");
-  pressed.className += "appear";
-  }, false);
-
-
 })();
